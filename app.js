7433b70843121f82d6d409208b4836a4974d76c3
Abhishek Dubey <abhishekdubey8126@.com>
2025-01-14 00:53:20 +0530

    fourth commit- added welcome page

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
+app.get('/register', (req, res) => {
+    res.render('register'); 
+});
+
 app.get('/login', (req, res) => {
     res.render("login");
 });
--- models/user.js
+++ models/user.js
@@ -4,8 +4,6 @@ mongoose.connect("mongodb://localhost:27017/data_association_project");
 
 const userSchema = mongoose.Schema({
     username: String,
-    name: String,
-    age:Number,
     email:String,
     password: String,
     posts:[{type: mongoose.Schema.Types.ObjectId, ref:"post"}]
